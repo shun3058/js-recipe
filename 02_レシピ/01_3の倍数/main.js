1c8cbcda6775f8c44af5b92fda094d62ca42f6dc
shun3058 <greeeen.1004.1004@gmail.com>
2024-05-09 15:47:38 +0900

    FizzBuzzできました

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -10,4 +10,21 @@ const genkiFunction = function (number) {
   }
 }
 
-console.log(genkiFunction(100))
+console.log(genkiFunction(10))
+
+//FizzBuzz
+const FizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+console.log(FizzBuzz(35))
