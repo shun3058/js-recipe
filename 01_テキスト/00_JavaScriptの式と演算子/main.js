32d66cbd891bcc77cc5f5c9fa972ebd093d20a2d
shun3058 <greeeen.1004.1004@gmail.com>
2024-05-09 14:32:27 +0900

    式と演算子

--- 01_テキスト/00_JavaScriptの式と演算子/main.js
+++ 01_テキスト/00_JavaScriptの式と演算子/main.js
@@ -1,7 +1,14 @@
 // TRY: 練習問題１
-
+console.log(57052 + 57054)
+console.log("114" + "106")
 // TRY: 練習問題２
+let x = 5
+x += 2
+x *= 5
+x = 100 % x
+x /= 6
 
+console.log(x)
 // TRY: 練習問題３
 let happiness = 0 // 0 から 100 までの数値を代入してください
 let sleepiness = 0 // 0 から 100 までの数値を代入してください
@@ -12,6 +19,9 @@ const isSleepy = sleepiness > 90
 const isNotHappyAndSleepy = !isHappy && isSleepy
 
 // 以下のコメントアウトを外して確認してみましょう
-// console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
-// console.log("isHappy", isHappy)
-// console.log("isSleepy", isSleepy)
+console.log(
+  "isNotHappyAndSleepy" + "は" + true + "ですか？",
+  isNotHappyAndSleepy,
+)
+console.log("isHappy", isHappy)
+console.log("isSleepy", isSleepy)
