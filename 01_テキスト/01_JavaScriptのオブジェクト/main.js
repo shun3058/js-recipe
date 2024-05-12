cc8bcf487433c723a3e65ab62435a69b00b61fe9
shun3058 <greeeen.1004.1004@gmail.com>
2024-05-12 14:13:18 +0900

    [オブジェクト]を学習しました
    ”

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -16,11 +16,20 @@ const hikakin = {
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
 
-// const me = { name: 'あなたのなまえ' }
+const me = {
+  name: "大山駿",
+  age: 20,
+  hobbies: ["音楽鑑賞", "YouTube"],
+  address: {
+    country: "日本",
+    city: "室蘭",
+  },
+}
 
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
 
-// console.log(me)
+console.log(me)
 
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
